ab3987c3ae3af08bf3bbf95f58054f00d9b431d3
Georgy Marchuk <gmarcuk@gmail.com>
2019-02-13 18:16:51 +0100

    init

---
+++ readme.md
@@ -0,0 +1,93 @@
+# Nette helpers
+
+## Prerequisites
+Include `setComponents` function to define components for replaced snippets.
+```javascript
+import { setComponents } from '@giantcz/nette';
+setComponents(components);
+``` 
+
+## Functions
+### `request`
+Sends request and automatically processes the response. 
+
+In a JSON response:
+* `snippets` get replaced and [Gia components](https://github.com/giantcz/gia#component) within are initialized 
+* `action` gets triggered in [Gia eventbus](https://github.com/giantcz/gia#eventbus) in a form of `nette:[action string]`
+* `url` gets replaced in the URL bar with [replaceState](https://developer.mozilla.org/en-US/docs/Web/API/History_API#The_replaceState()_method)
+
+Accepts options object with defaults:
+```javascript
+const options = {
+    url: window.location.pathname + window.location.search,
+    method: "GET",
+    data: null
+}
+request(options);
+```
+
+Function returns Promise of the request that can be further used for processing.
+```javascript
+request(options)
+    .then(({response, request}) => {
+        console.log(response);
+        console.log(request);
+    })
+    .catch(request => {
+        console.error(request);
+    });
+```
+
+### `send`
+Accepts element (`a` or `form`) and makes Ajax request.  
+For `a`, request is made to the URL defined in `href` attribute. 
+For `form`, request is with a form submission. 
+In case any `form` includes any element with class `has-error`, the sending is cancelled.
+
+Function also returns Promise of the request.
+
+### `replaceSnippet`
+Replaces the content of HTML snippets and initializes [Gia components](https://github.com/giantcz/gia#component) within.
+
+Accepts:
+* `newContent` - string of new content
+* `snippetName` - snippet name (`id` attribute)
+
+Example: 
+```javascript
+replaceSnippet('<div>Content</div>', 'snippet--paginator');
+```
+
+### `replaceSnippets`
+Accepts object and runs [replaceSnippet](#replaceSnippet) for every record. 
+```javascript
+replaceSnippets({
+    'snippets--paginator': '<div>Content</div>',
+});
+```
+
+### `setComponents`
+Saves component constructors for further use when replacing snippets. 
+Function accepts object of components.
+
+```javascript
+import { setComponents } from '@giantcz/nette';
+setComponents(components);
+``` 
+
+### `triggerEvent`
+Triggers event in [Gia eventbus](https://github.com/giantcz/gia#eventbus) in a form of `nette:[first parameter]`.
+```javascript
+triggerEvent("openPopup");
+```
+
+### `Snippet`
+When initialized over an element, the instance is saved inside of the element. If the instance exists, it's `update` method is used for processing of the content replacement of snippet.
+```javascript
+import { Snippet } from '@giantcz/nette';
+const instance = new Snippet(element);
+
+instance.update = function({ element, newContent, snippetName }) {
+    ...
+}
+```
---
+++ src/index.js
@@ -0,0 +1,213 @@
+import eventbus from 'gia/eventbus';
+import loadComponents from 'gia/loadComponents';
+import removeComponents from 'gia/removeComponents';
+import config from 'gia/config';
+
+const store = {};
+
+export function send(element) {
+    if (element.tagName.toLocaleLowerCase() === "form") {
+        return submit(element);
+    } else if(element.tagName.toLocaleLowerCase() === "a") {
+        return click(element);
+    } else {
+        console.warn('Element is not tag "a" or "form"');
+    }
+}
+
+export function request(options) {
+    return new Promise((resolve, reject) => {
+        let defaults = {
+            url: window.location.pathname + window.location.search,
+            method: "GET",
+            data: null
+        };
+
+        let data = {
+            ...defaults,
+            ...options
+        };
+
+        let request = new XMLHttpRequest();
+
+        request.onreadystatechange = function() {
+            if (request.readyState === 4) {
+                if (request.status !== 500) {
+                    try {
+                        let json = JSON.parse(request.responseText);
+                        resolve({
+                            response: json,
+                            request: request,
+                        });
+                    } catch(error) {
+                        resolve({
+                            response: request.responseText,
+                            request: request,
+                        });
+                    }
+                } else {
+                    reject(request);
+                }
+            }
+        }
+
+        request.open(data.method, data.url, true);
+        request.setRequestHeader("X-Requested-With", "XMLHttpRequest");
+        request.send(data.data);
+    });
+}
+
+export function click(element) {
+    element.classList.add('is-loading');
+
+    const connection = request({
+        url: element.href
+    });
+    connection.then(({response, request}) => {
+        element.classList.remove('is-loading');
+        if (typeof response === 'object') {
+            processResponse(response);
+        }
+    }).catch(error => {
+        console.warn(error);
+    });
+    return connection;
+}
+
+export function submit(element) {
+    if (queryAll('.has-error', element).length) {
+        console.warn('Form not valid.');
+        return;
+    }
+
+    element.classList.add('is-loading');
+
+    // submitted
+    let form = element;
+    let formData = new FormData(form);
+
+    // send data
+    const connection = request({
+        url: form.action,
+        method: form.method,
+        data: formData,
+    });
+    connection.then(({response, request}) => {
+        element.classList.remove('is-loading');
+        if (typeof response === 'object') {
+            processResponse(response);
+        }
+    }).catch(error => {
+        console.warn(error);
+    });
+    return connection;
+}
+
+function processResponse(response) {
+    if (response.action) {
+        triggerEvent(response.action);
+    }
+    if (response.snippets) {
+        replaceSnippets(response.snippets);
+    }
+    if (response.url) {
+        window.history.replaceState({
+                url: response.url,
+                random: Math.random(),
+                source: "swup",
+            },
+            document.title,
+            response.url,
+        );
+    }
+}
+
+export function replaceSnippets(snippets) {
+    Object.keys(snippets).forEach(snippetName => {
+        replaceSnippet(snippets[snippetName], snippetName);
+    });
+}
+
+export function replaceSnippet(newContent, snippetName) {
+    const element = document.getElementById(snippetName);
+    const customSnippet = element['__nette_snippet__'];
+
+    if (customSnippet) {
+        customSnippet._replaceContent();
+    } else {
+        removeComponents(element);
+
+        if (element.hasAttribute("data-ajax-append")) {
+            element.innerHTML = element.innerHTML + newContent;
+        } else if (element.hasAttribute("data-ajax-prepend")) {
+            element.innerHTML = newContent + element.innerHTML;
+        } else if (element.innerHTML !== newContent) {
+            element.innerHTML = newContent;
+        }
+
+        loadComponents(store.components, element);
+
+        if (config._options.log) {
+            console.info(`Updated snippet '${ snippetName }'`);
+        }
+    }
+}
+
+export function triggerEvent(action) {
+    eventbus.emit(`nette:${action}`);
+}
+
+export class Snippet {
+    constructor(element) {
+        this.element = element;
+        this.element['__nette_snippet__'] = this;
+    }
+
+    _replaceContent({ element, newContent, snippetName }) {
+        removeComponents(element);
+        this.update({ element, newContent, snippetName });
+        loadComponents(element);
+    }
+
+    update({ element, newContent, snippetName }) {
+        removeComponents(element);
+
+        if (element.hasAttribute("data-ajax-append")) {
+            element.innerHTML = element.innerHTML + newContent;
+        } else if (element.hasAttribute("data-ajax-prepend")) {
+            element.innerHTML = newContent + element.innerHTML;
+        } else if (element.innerHTML !== newContent) {
+            element.innerHTML = newContent;
+        }
+
+        loadComponents(store.components, element);
+
+        if (config._options.log) {
+            console.info(`Updated snippet '${ snippetName }'`);
+        }
+    }
+}
+
+export function setComponents(components) {
+    store.components = components;
+}
+
+function queryAll(selector, context = document) {
+    if (typeof selector !== 'string') {
+        return selector;
+    }
+
+    return Array.prototype.slice.call(context.querySelectorAll(selector));
+}
+
+export default {
+    send: send,
+    request: request,
+    handleClick: click,
+    handleSubmit: submit,
+    replaceSnippets: replaceSnippets,
+    replaceSnippet: replaceSnippet,
+    triggerEvent: triggerEvent,
+    setComponents: setComponents,
+    Snippet: Snippet,
+}
